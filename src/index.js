44b6d26b62c7445a171a9878f6737c92a1c23018
Ravi Ojha <rbojha008@gmail.com>
2017-03-09 22:01:27 +0530

    Throttled searching

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from "youtube-api-search";
 import { SearchBar } from "./components/search" 
 import { VideoList } from "./components/video_list";
 import { VideoDeatil } from "./components/video_detail"
+import * as _ from "lodash";
 
 const API_KEY = "AIzaSyD6ZbIYduRD5l30XHDx2MATlGIPQwha3HU";
 
@@ -20,8 +21,9 @@ class App extends React.Component {
 	}
 
 	render() {
+		const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
 		return <div>
-			<SearchBar onSearchTermChange={term => this.videoSearch(term)}></SearchBar>
+			<SearchBar onSearchTermChange={videoSearch}></SearchBar>
 			<VideoDeatil video={this.state.selectedVideo}></VideoDeatil>
 			<VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}></VideoList>
 		</div>;
