fe2da8d6d13eccfd605478c7468397c448fb3eea
Ravi Ojha <rbojha008@gmail.com>
2017-03-09 19:44:22 +0530

    Getting youtube ready - 1

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,24 @@
 import * as React from "react";
 
 export class SearchBar extends React.Component {
-	constructor() {
-		super();
+	handleOnChangeCurry = this.handleOnChange.bind(this);
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			term: ""
+		};
 	}
 
 	render() {
-		return <input placeholder="Enter to search here"></input>;
+		return <div>
+			<input placeholder="Enter to search here" onChange={this.handleOnChangeCurry} value={this.state.term}></input>
+		</div>;
+	}
+
+	handleOnChange(e) {
+		this.setState({
+			term: e.target.value
+		});
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 import { SearchBar } from "./components/search" 
+import { VideoList } from "./components/video_list";
+import { VideoDeatil } from "./components/video_detail"
 
-const API_KEy = "AIzaSyD6ZbIYduRD5l30XHDx2MATlGIPQwha3HU"
+const API_KEY = "AIzaSyD6ZbIYduRD5l30XHDx2MATlGIPQwha3HU";
 
-// Create a new component. This should component should produce some HTML.
-const App = () => {
-	return <div>
-		<SearchBar></SearchBar>
-	</div>;
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: []
+		};
+
+		YTSearch({key: API_KEY, term: "surfboard"}, videos => {
+			this.setState({
+				videos
+			});
+		});
+	}
+
+	render() {
+		return <div>
+			<SearchBar></SearchBar>
+			<VideoDeatil video={this.state.videos[0]}></VideoDeatil>;
+			<VideoList videos={this.state.videos}></VideoList>
+		</div>;
+	}
 }
 
-// Take this component's generated HTMl and shove it into DOM
-ReactDOM.render(<App/>, document.querySelector(".container"));
+ReactDOM.render(<App />, document.querySelector(".container"));
