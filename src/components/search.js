9f7b316a8ebc009411eb7f51b87223db35fb49cc
Ravi Ojha <rbojha008@gmail.com>
2018-03-29 12:56:49 +0530

    Some more changes

--- src/components/search.js
+++ src/components/search.js
@@ -1,26 +1,28 @@
-import * as React from "react";
+import * as React from 'react';
 
 export class SearchBar extends React.Component {
-	handleOnChangeCurry = this.handleOnChange.bind(this);
-	constructor(props) {
-		super(props);
+  handleOnChangeCurry = this.handleOnChange.bind(this);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			term: ""
-		};
-	}
+    this.state = {
+      term: ''
+    };
+  }
 
-	render() {
-		return <div className="search-bar">
-			<input placeholder="Enter to search here" onChange={this.handleOnChangeCurry} value={this.state.term}></input>
-		</div>;
-	}
+  render() {
+    return (
+      <div className="search-bar">
+        <input placeholder="Enter to search here" onChange={this.handleOnChangeCurry} value={this.state.term} />
+      </div>
+    );
+  }
 
-	handleOnChange(e) {
-		this.setState({
-			term: e.target.value
-		});
+  handleOnChange(e) {
+    this.setState({
+      term: e.target.value
+    });
 
-		this.props.onSearchTermChange(e.target.value)
-	}
+    this.props.onSearchTermChange(e.target.value);
+  }
 }
